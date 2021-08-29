d88a42b4de45cfb189d1e6b8a439c20c63f0d68d
praveen.ram.kannan <praveen.ram.kannan@gmail.com>
2021-08-29 23:02:06 +0530

    corrected code with PEP8 standards

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -6,7 +6,7 @@ file = '../data/bit_coin_data.csv'
 url = 'https://storage.googleapis.com/zalora-interview-data/bitstampUSD_1-min_data_2012-01-01_to_2020-09-14.csv'
 
 
-def download(header):
+def request_api(header):
     '''
     Download data with the particular range mentioned in the header object
     :param header:
@@ -22,27 +22,31 @@ def download(header):
 
 def download_bitcoin_data():
     '''
-    Since the size of the dataset is too large, if all the contents are fetched at once, the process will be slower
+    Since the size of the dataset is too large, if all the contents are fetched
+     at once, the process will be slower
     Therefore multithreading is used, to capture chunks of data in parallel
 
     Approach :
     1. Split the entire data into 100 MB chunks
-    2. Since the file is arround 280 MB, used 3 threads to run in parallel that fetches 100 MB of data each
-    3. Headers used in the get request is created in such a way that the data is not duplicated
-    4. Once the chunk is fetched, the chunk is written to the output data file in parallel
+    2. Since the file is arround 280 MB, used 3 threads to run in parallel
+    that fetches 100 MB of data each
+    3. Headers used in the get request is created in such a way that the data
+    is not duplicated
+    4. Once the chunk is fetched, the chunk is written to the output data file
+    in parallel
     :return:
     '''
     chunk_size = 104857600  # 100 MB chunks
     headers = []
     _start, _stop = 0, 0
 
-    for x in range(3):  # file size is > 250MB, so we download in 3 parts with 100MB each.
+    for x in range(3):
         _start = _stop
         _stop = chunk_size * (x + 1)
         headers.append({"Range": f"bytes={_start}-{_stop}"})
 
     for header in headers:
-        t = Thread(target=download, args=(header,))
+        t = Thread(target=request_api, args=(header,))
         t.start()
         t.join()
     return True
